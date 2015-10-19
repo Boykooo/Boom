ca35cf3a2c914a17ac57e498982f9a2a397238d5
Boykooo <Boykooo@bk.ru>
2015-10-19 20:20:19 +0300

    Добавил регистрацию

---
+++ Client/RegistrationForm.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Server;
+using System.Net.Sockets;
+using Project2;
+
+namespace Client6
+{
+    public partial class RegistrationForm : Form
+    {
+        public string Nick { get; set; }
+        public RegistrationForm()
+        {
+            InitializeComponent();
+        }
+        private void RegistrationForm_Load(object sender, EventArgs e)
+        {
+        }
+        private void EndRegistration_Click(object sender, EventArgs e)
+        {
+            if (Check.CheckNickName(NickName.Text))
+            {
+                int port = 8888;
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress adress = host.AddressList[0];
+                IPEndPoint endPoint = new IPEndPoint(adress, port);
+                Socket socket = new Socket(adress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+                socket.Connect(endPoint);
+                socket.Send(MessageSerializer.Serialize(new RegistrationMessage(NickName.Text)));
+                byte[] temp = new byte[1000000];
+                socket.Receive(temp);
+                RegistrationResultMessage m = (RegistrationResultMessage)MessageSerializer.Deserialize(temp);
+                if (m.ok)
+                {
+                    this.Close();
+                }
+            }
+        }
+    }
+}
