7e9bfaa301129655444fb603fc7863a2a5c61e4a
Vlad <111@11.ru>
2015-12-04 15:30:01 +0300

    Исправление окна ожидания

---
+++ Client/Game/WaitForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Client.Game
+{
+    public partial class WaitForm : Form
+    {
+        public WaitForm()
+        {
+            InitializeComponent();
+        }
+
+        private void WaitForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void WaitForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
